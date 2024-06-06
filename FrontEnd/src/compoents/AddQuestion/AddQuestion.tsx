dae66653f9cc27f5966de59ef5e1bfb2f96c3789
Almas Shaikh <almas.shaikh@radixweb.com>
2024-06-06 19:28:39 +0530

    Somw work in Frontend side

--- FrontEnd/src/compoents/AddQuestion/AddQuestion.tsx
+++ FrontEnd/src/compoents/AddQuestion/AddQuestion.tsx
@@ -1,25 +1,150 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AddQuestion = () => {
+  const [setSubject, setShowSubject] = useState(null)
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/subject/getSubject', {
+          headers: {
+            'Authorization': localStorage.getItem('token'),
+            'Content-Type': 'application/json',
+          }
+        });
+        setShowSubject(response.data);
+      } catch (err) {
+        console.log("Err while Fetching Data", err)
+      }
+    }
+    fetchdata()
+  }, [])
+  type no_of_question = {
+    subjectName: String,
+    totalQuestion: number
+  }
+
+  const [questionForm, setquestionForm] = useState<no_of_question>({
+    subjectName: "",
+    totalQuestion: 0
+  })
+
+  const handleSubject = (e: any) => {
+    const { name, value } = e.target
+    setquestionForm({ ...questionForm, [name]: value })
+  }
+
+  const setquestion = (e: any) => {
+    const { name, value } = e.target
+    setquestionForm({ ...questionForm, [name]: value })
+  }
+  console.log(questionForm)
+  const validation = () => {
+    let flag = 0
+    if (questionForm.subjectName.length == 0 && questionForm.subjectName === "Select Subject") {
+      document.getElementById('subjectNameErr').innerHTML = 'please select subject'
+    } else {
+      flag = flag + 1
+      document.getElementById('subjectNameErr').innerHTML = ''
+    }
+    if (questionForm.totalQuestion == 0) {
+      document.getElementById('subjectQuestionErr').innerHTML = 'please Enter No of Question'
+    } else {
+      flag = flag + 1
+      document.getElementById('subjectQuestionErr').innerHTML = ''
+    }
+    return flag
+  }
+  const data = JSON.stringify(questionForm)
+  console.log(typeof(data))
+  const handlesubmit = (e: any) => {
+    e.preventDefault()
+    let valid = validation()
+    if (valid == 2) {
+      console.log(questionForm)
+    }
+  }
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col-3">
-          <form action="" className="mt-2">
-            <select className="form-select mt-3 px-2 p-2">
-              <option selected>Select Subject</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-            
-                <label htmlFor="question" className="px-1">Enter no of Question</label>
-                <input type="text" className="form-control px-2" name="noOfQuestion" placeholder="Enter of Question you want to Create" />
-                <button type="submit" className="btn btn-primary mt-2">Add Question Fileds</button>
+            <form onSubmit={(e) => handlesubmit(e)} className="mt-2">
+              <select className="form-select mt-3 px-2 p-2" id="subjectName" name="subjectName" onChange={(e) => handleSubject(e)}>
+                <option value="Select Subject">Select Subject</option>
+                {setSubject && setSubject.map((value, idx) => (
+                  <option key={idx} value={value.subjectName}>{value.subjectName}</option>
+                ))}
+              </select>
+              <label id="subjectNameErr" className="px-1" style={{ color: "red" }}></label>
+              <label htmlFor="question" className="px-1">Enter no of Question</label>
+              <input type="text" name="totalQuestion"
+                className="form-control px-2"
+                placeholder="Enter of Question you want to Create"
+                onChange={(e) => setquestion(e)} />
+              <label id="subjectQuestionErr" className="px-1" style={{ color: "red" }}></label>
+
+              <button type="submit" className="btn btn-primary mt-2">Add Question Fileds</button>
             </form>
           </div>
+        
 
+          <div className="col-9 mt-2">
+            {
+              JSON.parse(questionForm).map((value,index)=>{
+                <>
+               
+                <label>Question-{index + 1}</label>
+                <input type="text" className="form-control" placeholder="Enter Question" />
+                <div className="row g-3 mt-1">
+                  <div className="col-sm mb-2">
+                    <input type="text" className="form-control" placeholder="Enter Option-1" aria-label="City" />
+                  </div>
+                  <div className="col-sm">
+                    <input type="text" className="form-control" placeholder="Enter Option-2" aria-label="State" />
+                  </div>
+                  <div className="col-sm">
+                    <input type="text" className="form-control" placeholder="Enter Option-3" aria-label="Zip" />
+                  </div>
+                  <div className="col-sm">
+                    <input type="text" className="form-control" placeholder="Enter Option-4" aria-label="Zip" />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="Answer" className="ml-1">SelectAnswer</label>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="selectanswer" id="flexRadioDefault2" />
+                    <label className="form-check-label">
+                      Option-1
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="selectanswer" id="flexRadioDefault2" />
+                    <label className="form-check-label">
+                      Option-2
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="selectanswer" id="flexRadioDefault2" />
+                    <label className="form-check-label">
+                      Option-3
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input className="form-check-input" type="radio" name="selectanswer" id="flexRadioDefault2" />
+                    <label className="form-check-label">
+                      Option-4
+                    </label>
+                  </div>
+                </div>
+                </>
+              })
+            }
+            
+        
+
+
+          </div>
         </div>
       </div>
     </div>
--- FrontEnd/src/compoents/Login/Login.tsx
+++ FrontEnd/src/compoents/Login/Login.tsx
@@ -1,23 +1,78 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+    type user_login ={
+        userName:string,
+        userPassword:string
+    }
+    const [userlogin,setuserlogin] = useState<user_login>({
+        userName:"",
+        userPassword:""
+    })
+    const handleSting =(e:any) =>{
+        const {name,value} = e.target
+        setuserlogin({...userlogin,[name]:value})
+    }
+    
+    console.log(userlogin)
+    const validation = () =>{
+        let flag =0 
+        if(userlogin.userName.length == 0 || userlogin.userName.length <=4 ){
+            document.getElementById('userNameErr').innerHTML = 'Please Enter Valid Username'
+        }else{
+            flag = flag + 1
+            document.getElementById('userNameErr').innerHTML = ''
+        }
+        console.log(userlogin.userPassword.length)
+        if(userlogin.userPassword,length == 0 && userlogin.userPassword.length <= 8){
+            document.getElementById('userPasswordErr').innerHTML = 'Please Enter Valid password'
+        }else{
+            flag = flag + 1
+            document.getElementById('userPasswordErr').innerHTML = ''
+        }
+        return flag
+    }
+
+    const handlesubmit = (e:any) =>{
+        e.preventDefault()
+        const valid = validation()
+        console.log(valid);
+        
+        if(valid == 2){
+            axios.post('http://localhost:3000/user/login',{...userlogin})
+            .then((res)=>{
+                const token = res.data
+                localStorage.setItem('token', token);
+            })
+            .then(()=>{
+                setuserlogin({
+                    userName:"",
+                    userPassword:""
+                })
+            })
+            .catch((err)=>console.log(err))
+        }
+    }
     return (
         <div>
             <div className="container centered-form mt-2">
                 <div className="row">
                     <div className="col-md-6 offset-md-3 border mt-2">
-                        <form>
+                        <form onSubmit={(e)=>handlesubmit(e)}>
 
                             <div className="form-group">
                                 <label>UserName</label>
-                                <input type="text" className="form-control" placeholder="Enter Username" />
+                                <input type="text" name='userName' className="form-control" placeholder="Enter Username" onChange={(e)=>handleSting(e)} />
+                                <label id='userNameErr' style={{color:"red"}}></label>
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                <input type="password" name='userPassword' className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={(e)=>handleSting(e)}/>
+                                <label id='userPasswordErr' style={{color:"red"}}></label>
                             </div>
                             <div className="d-grid gap-2 col-6 mx-auto mt-3 mb-3">
-                                <button className="btn btn-primary" type="button">Login</button>
+                                <button className="btn btn-primary" type="submit">Login</button>
                             </div>
                         </form>
                     </div>
